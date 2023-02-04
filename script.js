c820a4c926ecd718c5aaf11ec834815d4af9529c
Noobamwere <124442013+Noobamwere@users.noreply.github.com>
2023-02-04 21:38:00 +0100

    This is the ready to test file
    Upload to fleek is for test reasons aproved.

---
+++ script.js
@@ -0,0 +1,27 @@
+// let button = document.querySelector("button");
+// let body = document.querySelector("body");
+
+// button.addEventListener("click", function() {
+//   if (body.style.backgroundColor === "white") {
+//     body.style.backgroundColor = "black";
+//   } else {
+//     body.style.backgroundColor = "white";
+//   }
+// });
+
+let button = document.querySelector("button");
+let body = document.querySelector("body");
+
+button.addEventListener("click", function() {
+  let color = getRandomColor();
+  body.style.backgroundColor = color;
+});
+
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
