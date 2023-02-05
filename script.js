fea8c9a98e3e738f4168f5622d26fce33058a5b9
Noobamwere <124442013+Noobamwere@users.noreply.github.com>
2023-02-05 13:08:09 +0100

    final edition 1.0

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-let button = document.querySelector("button");
-let body = document.querySelector("body");
+// let button = document.querySelector("button");
+// let body = document.querySelector("body");
 
-button.addEventListener("click", function() {
-  let color = getRandomColor();
-  body.style.backgroundColor = color;
-});
+// button.addEventListener("click", function() {
+//   let color = getRandomColor();
+//   body.style.backgroundColor = color;
+// });
 
-function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+// function getRandomColor() {
+//   let letters = "0123456789ABCDEF";
+//   let color = "#";
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
